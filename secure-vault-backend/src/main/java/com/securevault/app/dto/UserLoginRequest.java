44cbec4b7e759189662dddfc77483686d604aed2
Sumit Mochahary <sumitmochahary5@gmail.com>
2025-07-06 00:43:10 +0530

    Added UserLoginRequest in dto

---
+++ secure-vault-backend/src/main/java/com/securevault/app/dto/UserLoginRequest.java
@@ -0,0 +1,18 @@
+package com.securevault.app.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginRequest {
+
+    @NotBlank(message = "Email cannot be blank")
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    private String password;
+}
