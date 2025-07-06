6a719bf2f669c00e51269dcc397074bc2e07c5d9
Sumit Mochahary <sumitmochahary5@gmail.com>
2025-07-07 01:01:59 +0530

    Add JWT dependencies and initialize JwtService class

--- secure-vault-backend/src/main/java/com/securevault/app/service/JwtService.java
+++ secure-vault-backend/src/main/java/com/securevault/app/service/JwtService.java
@@ -1,4 +1,12 @@
 package com.securevault.app.service;
 
+import com.securevault.app.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class JwtService {
+
 }
