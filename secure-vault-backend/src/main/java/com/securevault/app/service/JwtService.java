b4ecd031a42011f2e4ed6035fd7cd2c50004c991
Sumit Mochahary <sumitmochahary5@gmail.com>
2025-07-08 22:34:57 +0530

    Implement JWT token generation and validation in JwtService; add JWT configuration to application.properties

--- secure-vault-backend/src/main/java/com/securevault/app/service/JwtService.java
+++ secure-vault-backend/src/main/java/com/securevault/app/service/JwtService.java
@@ -1,12 +1,64 @@
 package com.securevault.app.service;
 
 import com.securevault.app.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.crypto.SecretKey;
+import java.util.Date;
 
 @Service
 public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKeyString;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpirationMs;
+
+    private SecretKey secretKey;
+
+    @PostConstruct
+    public void init(){
+        byte[] keyBytes = Decoders.BASE64URL.decode(secretKeyString);
+        this.secretKey = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(User user){
+        return Jwts.builder()
+                .subject(user.getEmail())
+                .claim("email", user.getEmail())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(secretKey,Jwts.SIG.HS256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, User user){
+        String email = extractEmail(token);
+        return email.equals(user.getEmail()) && !isTokenExpired(token);
+    }
+
+    private String extractEmail(String token){
+        return parseClaims(token).getSubject();
+    }
+
+    private boolean isTokenExpired(String token) {
+        Date expiration = parseClaims(token).getExpiration();
+        return expiration.before(new Date());
+    }
+
+    private Claims parseClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
 
 }
